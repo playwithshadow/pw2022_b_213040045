eb345410d3b8013ae05300e1e7815dd38ddc37f3
rivaann <mrivans2002@gmail.com>
2022-05-18 20:21:21 +0700

    sudah mengerjakan sampai page blog

---
+++ tubes/blog2.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>VAN TECH</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/logo.png" />
+
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+
+    <!-- Bootstrap Core-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Css saya -->
+</head>
+
+<body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-lg">
+            <div class="container px-5">
+                <img src="assets/img/logo.png" alt="" width="40" height="40" />
+                <a class="navbar-brand" href="index.html">VAN TECHNOLOGY</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Perpustakaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="blog.html">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Galeri</a>
+                        </li>
+                    </ul>
+                    <div class="col-md-3 text-end">
+                        <a class="btn btn-outline-primary me-2" href="login/login.html">Login</a
+              >
+              <a class="btn btn-outline-primary" href="#">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- Blog -->
+        <section class="py-5 ">
+            <div class="container px-5 mt-lg-4">
+                <h1 class="fw-bolder fs-5 mb-5">Van Technology Blog</h1>
+                <div class="card border-0 shadow rounded-3 overflow-hidden">
+                    <div class="card-body p-0">
+                        <div class="row gx-0">
+                            <div class="col-lg-6 col-xl-5 py-lg-5">
+                                <div class="p-4 p-md-5">
+                                    <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                    <div class="h2 fw-bolder">Article heading goes here</div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique delectus ab doloremque, qui doloribus ea officiis...</p>
+                                    <a class="stretched-link text-decoration-none" href="postblog2.html">
+                                        Read more
+                                        <i class="bi bi-arrow-right"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-xl-7">
+                                <div class="bg-featured-blog" style="background-image: url('https://dummyimage.com/700x350/343a40/6c757d')"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Blog -->
+
+        <!-- Beberapa Artikel -->
+        <section class="py-5">
+            <div class="container px-5">
+                <h2 class="fw-bolder fs-5 mb-4">Artikel Terkait</h2>
+                <div class="row gx-5">
+                    <div class="col-lg-4 mb-5">
+                        <div class="card h-100 shadow border-0">
+                            <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                <a class="text-decoration-none link-dark stretched-link" href="#!">
+                                    <div class="h5 card-title mb-3">Blog post title</div>
+                                </a>
+                                <p class="card-text mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 mb-5">
+                        <div class="card h-100 shadow border-0">
+                            <img class="card-img-top" src="https://dummyimage.com/600x350/adb5bd/495057" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">Media</div>
+                                <a class="text-decoration-none link-dark stretched-link" href="#!">
+                                    <div class="h5 card-title mb-3">Another blog post title</div>
+                                </a>
+                                <p class="card-text mb-0">This text is a bit longer to illustrate the adaptive height of each card. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 mb-5">
+                        <div class="card h-100 shadow border-0">
+                            <img class="card-img-top" src="https://dummyimage.com/600x350/6c757d/343a40" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
+                                <a class="text-decoration-none link-dark stretched-link" href="#!">
+                                    <div class="h5 card-title mb-3">The last blog post title is a little bit longer than the others</div>
+                                </a>
+                                <p class="card-text mb-0">Some more quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Artikel -->
+
+        <!-- Footer-->
+        <footer class="bg-dark py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto">
+                        <div class="small m-0 text-white">
+                            Copyright &copy; 2022 &middot; Tubes &middot; Muhamad Rivan Sahronie
+                        </div>
+                    </div>
+                    <div class="col-auto ms-auto">
+                        <a class="link-light" href="https://www.instagram.com/rivaann_/" target="_blank"><i class="bi bi-instagram"></i
+            ></a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light" href="https://github.com/rivaannn" target="_blank"><i class="bi bi-github"></i
+            ></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core theme JS-->
+        <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
---
+++ tubes/postblog.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>VAN TECH</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/logo.png" />
+
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+
+    <!-- Bootstrap Core-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Css saya -->
+</head>
+
+<body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-lg">
+            <div class="container px-5">
+                <img src="assets/img/logo.png" alt="" width="40" height="40" />
+                <a class="navbar-brand" href="index.html">VAN TECHNOLOGY</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Perpustakaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="blog.php">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Galeri</a>
+                        </li>
+                    </ul>
+                    <div class="col-md-3 text-end">
+                        <a class="btn btn-outline-primary me-2" href="#">Login</a>
+                        <a class="btn btn-outline-primary" href="#">Daftar</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- Post Blog -->
+        <section class="py-5">
+            <div class="container px-5 my-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-lg-9">
+                        <!-- Post content-->
+                        <article>
+                            <!-- Post header-->
+                            <header class="mb-4">
+                                <!-- Post title-->
+                                <h1 class="fw-bolder mb-1">Welcome to Blog Post!</h1>
+                                <!-- Post meta content-->
+                                <div class="text-muted fst-italic mb-2">January 1, 2022</div>
+                            </header>
+                            <!-- Preview image figure-->
+                            <figure class="mb-4"><img class="img-fluid rounded" src="assets/img/blog1-more.jpg" alt="..." /></figure>
+                            <!-- Post content-->
+                            <section class="mb-5">
+                                <p class="fs-5 mb-4">Science is an enterprise that should be cherished as an activity of the free human mind. Because it transforms who we are, how we live, and it gives us an understanding of our place in the universe.</p>
+                                <p class="fs-5 mb-4">The universe is large and old, and the ingredients for life as we know it are everywhere, so there's no reason to think that Earth would be unique in that regard. Whether of not the life became intelligent is a different
+                                    question, and we'll see if we find that.</p>
+                                <p class="fs-5 mb-4">If you get asteroids about a kilometer in size, those are large enough and carry enough energy into our system to disrupt transportation, communication, the food chains, and that can be a really bad day on Earth.</p>
+                                <h2 class="fw-bolder mb-4 mt-5">I have odd cosmic thoughts every day</h2>
+                                <p class="fs-5 mb-4">For me, the most fascinating interface is Twitter. I have odd cosmic thoughts every day and I realized I could hold them to myself or share them with people who might be interested.</p>
+                                <p class="fs-5 mb-4">Venus has a runaway greenhouse effect. I kind of want to know what happened there because we're twirling knobs here on Earth without knowing the consequences of it. Mars once had running water. It's bone dry today. Something
+                                    bad happened there as well.</p>
+                            </section>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- End Post Blog -->
+
+        <!-- Footer-->
+        <footer class="bg-dark py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto">
+                        <div class="small m-0 text-white">
+                            Copyright &copy; 2022 &middot; Tubes &middot; Muhamad Rivan Sahronie
+                        </div>
+                    </div>
+                    <div class="col-auto ms-auto">
+                        <a class="link-light" href="https://www.instagram.com/rivaann_/" target="_blank"><i class="bi bi-instagram"></i></a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light" href="https://github.com/rivaannn" target="_blank"><i class="bi bi-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core theme JS-->
+        <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
